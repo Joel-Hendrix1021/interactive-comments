b6731c4cae6d20fbc20b2547e29871dbd8ba9d4b
unknown <joelpimentel1995@gmail.com>
2022-01-09 09:17:40 -0500

    delete home page

--- src/components/comentHeader/CommentHeader.js
+++ src/components/comentHeader/CommentHeader.js
@@ -6,7 +6,7 @@ const CommentHeader = ({ comment }) => {
     const user = comment.user.username;
     return (
         <HeaderComts>
-            <Img src={comment.user.image.png} alt={comment.user.username} />
+            <Img src={comment.user.image.webp} alt={comment.user.username} />
             <TextUser>{comment.user.username}</TextUser>
             {user === "juliusomo" && <SpanYou>you</SpanYou>}
             <Time>{typeof comment.createdAt === "number" ? <TimeAgo date={comment.createdAt}/> : comment.createdAt}</Time>
